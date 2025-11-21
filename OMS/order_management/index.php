2c2576f7ca0f4f5f954a957102b4db6759554987
feitsolution <admin1@fardardomestic.com>
2025-11-21 18:59:41 +0530

    uppppp2

--- OMS/order_management/index.php
+++ OMS/order_management/index.php
@@ -1,8 +1,21 @@
 <?php
-// Start session
+// Start session at the very beginning
 session_start();
 
-// Always redirect to login page
-header("Location: http://localhost/order_management/dist/pages/login.phpd");
-exit();
+// Check if user is logged in
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    // If not logged in, redirect to login page
+    if (ob_get_level()) {
+        ob_end_clean();
+    }
+    header("Location: /order_management/dist/pages/login.php");
+    exit();
+} else {
+    // If logged in, redirect to dashboard
+    if (ob_get_level()) {
+        ob_end_clean();
+    }
+    header("Location: /order_management/dist/dashboard/index.php");
+    exit();
+}
 ?>
