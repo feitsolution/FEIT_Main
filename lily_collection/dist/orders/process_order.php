99d90c4e2623b36263721e59ea4d3cd09c327781
Nethmini799 <117433954+DreamyD20@users.noreply.github.com>
2025-12-05 12:29:08 +0530

    ob_clean

--- lily_collection/dist/orders/process_order.php
+++ lily_collection/dist/orders/process_order.php
@@ -7,11 +7,6 @@ if (session_status() == PHP_SESSION_NONE) {
 // Disable error reporting for production
 error_reporting(0);
 
-// Clear any existing output buffers
-while (ob_get_level()) {
-    ob_end_clean();
-}
-
 // Include the database connection file
 include($_SERVER['DOCUMENT_ROOT'] . '/lily_collection/dist/connection/db_connection.php');
 
@@ -1677,9 +1672,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 }
                 
                 // Clear any output buffers before redirect
-                // while (ob_get_level()) {
-                //     ob_end_clean();
-                // }
+              ob_clean();
                 
                 header("Location: download_order.php?id=" . $order_id);
                 exit();
