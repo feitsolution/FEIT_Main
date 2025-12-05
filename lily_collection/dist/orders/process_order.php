b5a6fe8e400824ab355ade9af74484896fcf047c
Nethmini799 <117433954+DreamyD20@users.noreply.github.com>
2025-12-05 12:33:19 +0530

    back to existing

--- lily_collection/dist/orders/process_order.php
+++ lily_collection/dist/orders/process_order.php
@@ -8,9 +8,9 @@ if (session_status() == PHP_SESSION_NONE) {
 error_reporting(0);
 
 // Clear any existing output buffers
-// while (ob_get_level()) {
-//     ob_end_clean();
-// }
+while (ob_get_level()) {
+    ob_end_clean();
+}
 
 // Include the database connection file
 include($_SERVER['DOCUMENT_ROOT'] . '/lily_collection/dist/connection/db_connection.php');
