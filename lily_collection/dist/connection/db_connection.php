74546984c07d393bb1e610fc272936b852bc068d
feitsolution <admin1@fardardomestic.com>
2025-12-10 21:38:02 +0530

    lily to oms db test change

--- lily_collection/dist/connection/db_connection.php
+++ lily_collection/dist/connection/db_connection.php
@@ -3,7 +3,7 @@
 $servername = getenv('DB_HOST');
 $username   = getenv('DB_USER');
 $password   = getenv('DB_PASS');
-$dbname     = getenv('DB_NAME_LILY_COLLECTION');
+$dbname     = getenv('DB_NAME');
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
