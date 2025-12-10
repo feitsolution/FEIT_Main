67e3b75980097da5bacbdf72f0b92cfe059f8913
feitsolution <admin1@fardardomestic.com>
2025-12-10 21:55:09 +0530

    db change back after test

--- lily_collection/dist/connection/db_connection.php
+++ lily_collection/dist/connection/db_connection.php
@@ -3,7 +3,7 @@
 $servername = getenv('DB_HOST');
 $username   = getenv('DB_USER');
 $password   = getenv('DB_PASS');
-$dbname     = getenv('DB_NAME');
+$dbname     = getenv('DB_NAME_LILY_COLLECTION');
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
