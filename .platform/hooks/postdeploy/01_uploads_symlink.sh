2ec7944d3a0487aa6523a01c8cbc4b1510add251
feitsolution <admin1@fardardomestic.com>
2026-01-19 16:27:30 +0530

    create symlink.sh

---
+++ .platform/hooks/postdeploy/01_uploads_symlink.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+SHARED="/var/app/shared/uploads"
+
+mkdir -p $SHARED
+chown -R webapp:webapp $SHARED
+chmod -R 775 $SHARED
+
+for APP in lily_collection 
+do
+  DIST="/var/app/current/$APP/dist/uploads"
+
+  rm -rf $DIST
+  ln -s $SHARED $DIST
+done
