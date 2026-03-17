a43157a7f07eecb9bf61958f13e2b553b03722a3
Pawan Perera <pawanperera5@gmail.com>
2026-03-17 17:00:49 +0530

    date_default_timezone_set("Asia/Colombo");

--- order_management/dist/connection/fe_it_db_connection.php
+++ order_management/dist/connection/fe_it_db_connection.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set("Asia/Colombo");
 // Database connection
 $fe_servername = getenv('DB_HOST');   // Database server
 $fe_username   = getenv('DB_USER');        // Database username (default for XAMPP is root)
