e608148e692820c633b759c81cfb069b5b239a51
Pawan Perera <pawanperera5@gmail.com>
2026-03-23 08:50:12 +0530

    Admin Test

--- admin/db_connection.php
+++ admin/db_connection.php
@@ -1,4 +1,6 @@
 <?php
+date_default_timezone_set("Asia/Colombo");
+
 // Database connection
 $servername = getenv('DB_HOST');
 $username   = getenv('DB_USER');
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Start session at the very beginning
 session_start();
 
