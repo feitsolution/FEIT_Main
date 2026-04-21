b3197b4d3e2d48f0d5bc20731918e9716f269a26
feitsolution <admin1@fardardomestic.com>
2026-04-22 01:09:50 +0530

    Revert "test"
    This reverts commit efab85a64bab5e7b1da317ee61a1aeda197c6083.

--- finger_print_int/run-finger-scanner-services.bat
+++ finger_print_int/run-finger-scanner-services.bat
@@ -4,7 +4,7 @@ echo Starting Laravel service and Python app...
 :: Start Laravel service in a new window
 start "Laravel Service" cmd /k "cd /d %~dp0ZKTeco-Finger-Scanner-feature-no-register && php artisan serve --host 0.0.0.0 --port=8000"
 
-:: Start Python wrapper in a new window 22
+:: Start Python wrapper in a new window
 start "Python App" cmd /k "cd /d %~dp0Finger-Scanner-ZKTeco-Wrapper-master && uv run app.py"
 
 echo Both services started in new windows.
